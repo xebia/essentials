397adb73513abc63a768fe30ff06a7c49e3c7a68
Joshua Appelman <joshua@jbna.nl>
2014-01-26 18:06:54 +0100

    Adds OverviewPage under /all.

--- _plugins/categories.rb
+++ _plugins/categories.rb
@@ -7,25 +7,46 @@ module Jekyll
       @dir = dir
       @name = 'index.html'
 
-
       self.process(@name)
         self.read_yaml(File.join(base, '_layouts'), 'list.html')
-      self.data['category'] = category
+      self.data['category'] = 'other'
       category_title_prefix = site.config['category_title_prefix'] || 'category: '
       self.data['title'] = "#{category_title_prefix}#{category}"
-      self.data['cards'] = ['alone-time']
+      self.data['cards'] = []
+      site.pages.each do |page|
+        if page.data['category'] == category
+          self.data['cards'] << page.name
+        end
+      end
+    end
+  end
+
+  class OverviewPage < Page
+    def initialize(site, name)
+      @site = site
+      @base = site.source
+      @dir = name
+      @name = 'index.html'
+      self.process(@name)
+        self.read_yaml(File.join(site.source, '_layouts'), 'list.html')
+      self.data['category'] = 'other'
+      self.data['title'] = 'all'
+      self.data['cards'] = []
+      site.pages.each do |page|
+        if page.data['category'] != 'other'
+          self.data['cards'] << page.basename
+        end
+      end
     end
   end
 
   class CategoryPageGenerator < Generator
     safe true
-
     def generate(site)
-      p 'a'
       site.config["card_types"].each do |category|
-        p category
         site.pages << CategoryPage.new(site, site.source, File.join("categories", category), category)
       end
+      site.pages << OverviewPage.new(site, 'all')
     end
   end
 
