07fc4fcd209412230004ee9db3c2f3c985b07855
Joshua Appelman <jappelman@xebia.com>
2014-05-30 01:45:12 +0200

    Removes generator for /all/ page.
    /cards/ now forfills the same purpose.

--- _plugins/categoriesGenerator.rb
+++ _plugins/categoriesGenerator.rb
@@ -21,32 +21,12 @@ module Jekyll
     end
   end
 
-  class OverviewPage < Page
-    def initialize(site, name)
-      @site = site
-      @base = site.source
-      @dir = name
-      @name = 'index.html'
-      self.process(@name)
-        self.read_yaml(File.join(site.source, '_layouts'), 'list.html')
-      self.data['category'] = 'other'
-      self.data['title'] = 'all'
-      self.data['cards'] = []
-      site.pages.each do |page|
-        if page.data && page.data['category'] != 'other'
-          self.data['cards'] << page.basename
-        end
-      end
-    end
-  end
-
   class CategoryPageGenerator < Generator
     safe true
     def generate(site)
       site.config["card_types"].each do |category|
         site.pages << CategoryPage.new(site, site.source, File.join("categories", category['name']), category['name'])
       end
-      site.pages << OverviewPage.new(site, 'all')
     end
   end
 
