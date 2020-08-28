be77de95ea5fd494b2f515613be7ecd454e836a0
Frank van Wijk <fvanwijk@xebia.com>
2020-08-28 16:21:22 +0200

    Set base href to make relative links absolute from root

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import CardLayout from '~/layouts/Card.vue';
 export default function(Vue, { router, head, isClient }) {
   Vue.component('CardLayout', CardLayout);
 
+  head.base = { href: '/' };
   head.link.push({
     href: '//fonts.googleapis.com/css?family=Open+Sans:400,600',
     rel: 'stylesheet',
